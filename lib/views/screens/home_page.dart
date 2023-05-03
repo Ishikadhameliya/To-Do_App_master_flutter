f1e720fa510c3080bdb2db613700864fee8964a5
Ishikadhameliya <ishikadhameliya@gmail.com>
2023-05-03 22:00:48 +0530

    First Update

--- lib/views/screens/home_page.dart
+++ lib/views/screens/home_page.dart
@@ -33,81 +33,100 @@ class _HomePageState extends State<HomePage> {
           }),
         ],
       ),
-      body: GetBuilder<DataController>(
-            builder: (_) {
-              return ListView.builder(
-                  itemCount: dataController.list.length,
-                  itemBuilder: (context, index) => Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Column(
-                          children: [
-                            Container(
-                              alignment: Alignment.center,
-                              height: 100,
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                gradient:
-                                LinearGradient(colors: [Colors.blue.shade800, Colors.greenAccent]),
-                              ),
-                              child: Row(
-                                children: [
-                                  const SizedBox(width: 20,),
-                                  CircleAvatar(
-                                    radius: 35,
-                                    backgroundColor: Colors.blue.shade500,
-                                    child: Image.asset("assets/images/woman.png",fit: BoxFit.fill),
-                                  ),
-                                  const SizedBox(width: 10,),
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      const Text(
-                                        "Hi, Ishika",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 25,
-                                            color: Colors.white),
-                                      ),
-                                      const Text(
-                                        "Here is your Today's Task",
-                                        style: const TextStyle(
-                                            fontWeight: FontWeight.w400,
-                                            fontSize: 17,
-                                            color: Colors.white),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              )
-                            ),
-                            const SizedBox(height: 20,),
-                            Row(
-                              children: [
-                                Container(
-                                  height: 8.h,
-                                  width: 20.w,
-                                  alignment: Alignment.center,
-                                  decoration: BoxDecoration(
-                                      border: Border.all(width: 5,color: Colors.black),
-                                      borderRadius: BorderRadius.circular(10)),
-                                  child: Text(
-                                    dataController.list[index].time,
-                                    style: const TextStyle(
-                                        fontSize: 20, fontWeight: FontWeight.bold),
-                                  ),
-                                ),
-                                SizedBox(width: 3.w),
-                                Expanded(child: dataController.list[index].body)
-                              ],
-                            ),
-                          ],
-                        ),
-                      ));
-            },
+      body: Column(
+        children: [
+          SizedBox(
+            height: 10,
           ),
-
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Container(
+                  alignment: Alignment.center,
+                  height: 100,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(
+                        colors: [Colors.blue.shade800, Colors.greenAccent]),
+                  ),
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundColor: Colors.blue.shade500,
+                        child: Image.asset("assets/images/woman.png",
+                            fit: BoxFit.fill),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            "Hi, Ishika",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            "Here is your Today's Task",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 17,
+                                color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            flex: 8,
+            child: GetBuilder<DataController>(
+              builder: (_) {
+                return ListView.builder(
+                    itemCount: dataController.list.length,
+                    itemBuilder: (context, index) => Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              Container(
+                                height: 8.h,
+                                width: 20.w,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        width: 5, color: Colors.black),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: Text(
+                                  dataController.list[index].time,
+                                  style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              SizedBox(width: 3.w),
+                              Expanded(child: dataController.list[index].body)
+                            ],
+                          ),
+                        ));
+              },
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           dataController.addToList();
